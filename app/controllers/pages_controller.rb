5c527d42029ac8ca73b7a0581bde5e5377077e8f
tranhuyhoangbka <hoangth92.nd@gmail.com>
2015-10-28 16:46:53 +0700

    using nokogiri

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,9 +6,14 @@ class PagesController < ApplicationController
     #@doc = Nokogiri::HTML(open("http://www.24h.com.vn/bong-da/ozil-la-vua-kien-tao-premier-league-va-chau-au-c48a744243.html"))
     #@doc = File.open("app/views/shared/_doc.html") { |f| Nokogiri::HTML(f) }
 
-    @doc = Nokogiri::XML(File.open("app/views/shared/_doc.html")) do |config|
+    @doc = Nokogiri::HTML(File.open("app/views/shared/_doc.html")) do |config|
       config.nonet
     end
     logger.debug "count h1 \n #{@doc.css('h1').count}"
+    @doc_xml = Nokogiri::XML(File.open("app/views/shared/shows.xml"))
+
+    @doc_xml2 = Nokogiri::XML(File.read("app/views/shared/parts.xml"))
+    @car_tires = @doc_xml2.xpath('//car:tire', 'car' => 'http://alicesautoparts.com/')
+    @bike_tires = @doc_xml2.xpath('//bike:tire', 'bike' => 'http://bobsbikes.com/')
   end
 end
